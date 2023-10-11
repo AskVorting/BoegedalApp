86fd2813c36845c950113a68f13ed35f95df5acc
Msitua <ask@boegedal.com>
2023-10-11 14:16:45 +0200

    yes

---
+++ BoegedalSrc/app/src/main/java/com/plcoding/BoegedalApp/dataList.kt
@@ -0,0 +1,11 @@
+package com.plcoding.BoegedalApp
+
+class dataList (
+    var nameOfBeer: String,
+    var typeOfBeer: String,
+    var alcoholContent: String,
+    var image : Int,
+    var description: String,
+    var price: String,
+
+)
