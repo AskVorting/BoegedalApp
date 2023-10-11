3080b93180135c6b28a7cff5f64cb329af7707df
Msitua <ask@boegedal.com>
2023-10-11 14:16:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/boegedalapp/beerScreen.kt
@@ -0,0 +1,38 @@
+
+
+package com.example.boegedalapp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun BeerScreen(navController: NavHostController){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Emil er lort og han er alt for høj",
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Center
+        )
+        ElevatedButton(onClick = { navController.navigate("home") }) {
+            Text(text = "Like")
+
+
+        }
+
+    }
+}
