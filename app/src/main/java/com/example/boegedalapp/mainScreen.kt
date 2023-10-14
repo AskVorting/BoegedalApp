2be1afc3dd6971525c726b623907b5ab60576a5f
Msitua <91263902+AskVorting@users.noreply.github.com>
2023-10-14 23:34:17 +0200

    50%
    fuldt fungerende app med firestore database

--- app/src/main/java/com/example/boegedalapp/mainScreen.kt
+++ app/src/main/java/com/example/boegedalapp/mainScreen.kt
@@ -1,6 +1,8 @@
 package com.example.boegedalapp
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -8,24 +10,37 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun HomeScreen(){
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+fun HomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "This is a app for bøgedal bryghus",
-            modifier = Modifier.padding(16.dp),
-            textAlign = TextAlign.Center
+        Image(
+            painter = painterResource(R.drawable.baggrund),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
         )
 
+        // Add text on top of the image
+        Text(
+            text = "welcome to Bøgedal Brew",
+            color = Color.White, // Set text color
+            fontSize = 24.sp, // Set text size
+            modifier = Modifier
+                .align(Alignment.Center) // Center the text
+        )
     }
 }
+
 
 
 @Composable
